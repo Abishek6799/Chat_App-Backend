f9b5ecf94329624ef326fbd3d293c1b6db49b066
Abishek6799 <abiabishek310@gmail.com>
2025-01-22 11:49:53 +0530

    updated

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app); 
 const io = new Server(server, {
-    cors: {
-        origin: "https://rainbow-chimera-925dab.netlify.app/",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+      origin: "https://rainbow-chimera-925dab.netlify.app", // No trailing slash
+      methods: ["GET", "POST"],
+      credentials: true,
+  },
 });
 
 app.use(cors());
